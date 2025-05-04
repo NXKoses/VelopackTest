0a75b624e99786195c8ac6fa37c06185f47efdd5
NX <nyannkasi@gmail.com>
2025-05-04 20:21:51 +0900

    初回起動時のウィンドウを設定した。

--- VelopackTest/Program.cs
+++ VelopackTest/Program.cs
@@ -12,7 +12,13 @@ namespace VelopackTest
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            VelopackApp.Build().Run();
+            VelopackApp.Build().WithBeforeUninstallFastCallback((v) => { }).WithFirstRun((v) =>
+            {
+                // ‰‰ñ‹N“®‚Ìˆ—
+                Form2 fm2 = new();
+                fm2.ShowDialog();
+            }).Run();
+
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
